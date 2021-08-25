7fbfbac93e9d62f6f2cc6c2b6b2c9797f38fc6b6
Gplgr <giovanni@LAPTOP-2MC464U0.localdomain>
2021-08-24 23:57:37 -0300

    first commit

---
+++ Semaforos/PCSem.c
@@ -0,0 +1,154 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+#include <string.h>
+#include <pthread.h>
+#include <semaphore.h>
+
+//Definindo a variavel global de contador pro limite de execucoes e os semaforos
+int impressaominha;
+sem_t empty,full,mutex;
+
+//Funcao que gera numero aleatorio entre 1 e 10000000
+int gerador(){
+    return (rand()%9999999)+1;
+};
+
+//Funcao que verifica se um numero e primo e printa
+//testa todos os valores ate a raiz do valor original; se nenhuma bater, e primo
+//existem divisores entre a raiz e a metade do valor, mas que so ocorrem caso algum inferior ocorra
+void printaprimo(int valorrecebido){
+    char *primo = malloc(256);
+    //usando o strcpy pra facilitar mexer com strings
+    strcpy(primo,"e primo");
+    if (valorrecebido==1) strcpy(primo,"nao e primo");
+    for (int j=2;(double)j <= sqrt(valorrecebido); j++){
+        if (valorrecebido%j == 0){
+            strcpy(primo,"Nao e primo");
+        };
+    };
+    printf("Em relacao a %d: %s\n", valorrecebido,primo);
+    free(primo);
+};
+
+//Funcao produtor
+void* produtor(void* arg){
+    int *querovaga = (int *) arg;
+    int N = sizeof(querovaga);
+    int escrevinhador;
+    while(impressaominha<10000){
+        //Gera o numero de forma independente
+        escrevinhador = gerador();
+        sem_wait(&empty);
+        sem_wait(&mutex);
+        //verifica onde tem espaco livre e salva no vetor
+        for (int i = 0; i < N; ++i){
+            if (querovaga[i] == 0){
+                querovaga[i] = escrevinhador;
+                escrevinhador = 0;
+                break;
+            };
+            //Se entrar aqui e porque nao achou vaga mesmo quando disseram que tinha
+            if (escrevinhador != 0) {
+                printf("Tem algo errado no produtor\n");
+            };
+        };
+        sem_post(&mutex);
+        sem_post(&full);
+        //Se ja tiverem terminado, para onde esta
+        if (impressaominha==10000){
+            pthread_exit(0);
+        };
+    }
+};
+
+//Funcao consumidor
+void* consumidor(void* arg){
+    int *queronumero = (int *) arg;
+    int leitao;
+    int N = sizeof(queronumero);
+    while(impressaominha<10000){
+        sem_wait(&full);
+        sem_wait(&mutex);
+        for (int i = 0; i < N; ++i){
+            if (queronumero[i] != 0){
+                leitao = queronumero[i];
+                queronumero[i] = 0;
+                break;
+            };
+            //Se entrar aqui e porque nao achou item mesmo quando disseram que tinha
+            if (leitao == 0) {
+                printf("Tem algo errado no consumidor\n");
+            };
+        };
+        //se alguem ja tiver chegado antes, desiste e sai
+        if (impressaominha==10000){
+            sem_post(&mutex);
+            sem_post(&empty);
+            pthread_exit(0);
+        };
+        //avisa que tem mais um sendo lido e impresso
+        impressaominha++;
+        sem_post(&mutex);
+        sem_post(&empty);
+        printaprimo(leitao);
+        leitao = 0;
+    };
+};
+
+
+int main (int argc, char *argv[]) {
+    //definindo os inputs
+    srand(time(NULL));
+    int N,Np,Nc;
+    printf("Insira o valor de N: ");
+    scanf("%d",&N);
+    printf("Insira o valor de Np: ");
+    scanf("%d",&Np);  
+    printf("Insira o valor de Nc: ");
+    scanf("%d",&Nc);
+    //Inicializando o vetor geral
+    int memoria[N];
+    //Por algum motivo nao estava passando 0 pros valores do vetor nem o valor direto nem com memset
+    //entao foi feita de forma iterativa
+    for (int i=0;i<N;i++){
+        memoria[i] = 0;
+    };
+    //Inicializando os semaforos
+    sem_init(&empty, 0, N);
+    sem_init(&full, 0, N);
+    sem_init(&mutex, 0, 1);
+
+    //Setando o valor inicial de empty em N
+    for (int i = 0; i < N; ++i){
+        sem_post(&empty);
+    };
+
+    clock_t begin = clock();
+    //Criando as threads
+    //Criando as produtoras
+    pthread_t produtores[Np];
+    for (int i=0;i<Np;i++){
+        pthread_create(&produtores[i],NULL,produtor, (void*)memoria);
+    };
+
+    //Criando as consumidoras
+        pthread_t consumidores[Nc];
+    for (int i=0;i<Nc;i++){
+        pthread_create(&consumidores[i],NULL,consumidor, (void*)memoria);
+    };
+
+    //Esperando a finalizacao das threads
+    for (int i=0;i<Np;i++){
+        pthread_join(produtores[i],NULL);
+    };
+    for (int i=0;i<Nc;i++){
+        pthread_join(consumidores[i],NULL);
+    };
+    clock_t end = clock();    
+    double tempo = (double)(end - begin) / CLOCKS_PER_SEC;
+    printf("O tempo total foi de %f\n", tempo);
+    return 0;
+};
---
+++ Spinlocks/Somador.c
@@ -0,0 +1,97 @@
+/* Somador.c */
+/* Giovanni Paes */
+/* Codigo para o exercicio de um somador com entradas aleatorias e paralelizado com spinlocks */
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+#include <pthread.h>
+#include <stdbool.h> 
+
+//estrutura do lock
+struct lock{
+    bool held;
+};
+
+//variaveis globais de soma e lock
+int soma = 0;
+struct lock lock;
+
+//tentei nao usar como global mas vi todos os erros possiveis tentando passar uma estrutura que continha
+//as tres variaveis anteriores e esse vetor como parametro pras threads
+//desisti e foi a melhor coisa que eu fiz
+int *vetor = NULL;
+
+//test and set conforme apresentado na aula
+bool test_and_set(bool *flag) {
+    bool anterior = *flag;
+    *flag = 1;
+    return anterior;
+}
+
+//funcao para adquirir o lock
+void acquire(struct lock lock) {
+    while(test_and_set(&lock.held));
+};
+
+//funcao para liberar o lock
+void release(struct lock lock) {
+    lock.held = 0;
+};
+
+//Funcao para as threads executarem
+void *funcaothreads(void *arg){
+    int *parametros = (int *) arg;
+    for (int i=parametros[2];i<parametros[0];i=i+parametros[1]){
+        acquire(lock);
+        soma = soma + vetor[i];
+        release(lock); 
+    };
+};
+
+//funcao principal
+int main(int argc, char *argv[]){
+    //setando as variáveis
+    int N,K;
+
+    //setando o valor inicial do lock
+    lock.held = 0;
+
+    //definindo os inputs
+    printf("Insira o valor de N: ");
+    scanf("%d",&N);
+    printf("Insira o valor de K: ");
+    scanf("%d",&K);
+
+    //setando o vetor de threads
+    pthread_t threads[K];
+
+    //Criando e retornando o vetor com os valores aleatorios
+    vetor = malloc(sizeof(int)*N);
+    srand(time(NULL));
+    int temp;
+    for(int i=0;i<N;i++){
+        vetor[i] = (rand() % 201) - 100;
+    };
+
+    //Criando as threads
+    clock_t begin = clock();
+    for (int i=0;i<K;i++){
+        int parametros[3] = {N,K,i};
+        int * p = malloc(3 * sizeof(int));
+        memcpy(p, parametros, 3*sizeof(int));
+        pthread_create(&threads[i],NULL,funcaothreads, p);
+    };
+
+    //Esperando a finalizacao das threads
+    for (int i=0;i<K;i++){
+        pthread_join(threads[i],NULL);
+    };
+    clock_t end = clock();
+    double tempo = (double)(end - begin) / CLOCKS_PER_SEC;
+    printf("A soma final e %d\n",soma);
+    printf("O tempo total foi de %f\n", tempo);
+    return 0;
+};
